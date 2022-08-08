cf1b77fd2580259d2ba7d2eb5296a9aac7d4f04a
MohamedB <m.m.bassiouni@gmail.com>
2022-08-08 10:41:53 -0700

    fixed bug in deployemnt-stash.sh

--- deployment-status.sh
+++ deployment-status.sh
@@ -3,6 +3,6 @@ if ! kubectl get ns mohamed-bassiouni; then
     kubectl create ns mohamed-bassiouni
 fi
 
-if ! kubectl rollout status deployment sample-spring-boot -n matthew-oberlies; then
+if ! kubectl rollout status deployment sample-spring-boot -n mohamed-bassiouni; then
     kubectl apply -f kubernetes.yml
 fi
